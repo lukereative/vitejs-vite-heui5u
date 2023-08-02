a878c5f2f96f79d83fd7e99750533fc945fdf5a0
lukereative <lexigraphic@gmail.com>
2023-08-02 16:17:23 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-heui5u
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-heui5u)
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useBreakpoints } from '@vueuse/core';
+const breakpoints = useBreakpoints({ lg: 1024 });
+console.log(breakpoints);
+</script>
+
+<template>
+  <h1>Hello VueUse!</h1>
+  <a
+    href="https://vueuse.org/"
+    target="_blank"
+    v-if="toValue(breakpoints.greaterOrEqual('lg'))"
+    >Documentation</a
+  >
+</template>
+
+<style></style>
